f7d33e3c012658395f7e92c6eab1a1a7c44b79a1
Rodolfo <rodolforodriguesjr@gmail.com>
2021-11-06 16:39:38 -0300

    First

---
+++ MavenProjectRodolfo/src/main/java/RodolfoMain.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author jrrod
+ */
+public class RodolfoMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ola");
+    }
+    
+}
